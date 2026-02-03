c70d58147e7f708707b859fefeaee143e3ec7b3e
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-02-03 14:42:21 +0530

    ..

---
+++ oopsJava/27-01-26/feb03/DataTypes.java
@@ -0,0 +1,16 @@
+package feb03;
+public class DataTypes {
+    public static void main(String[] args) {
+       
+        // for(int i=0;i<6;i++){
+        //     System.out.println("Hello " );
+        // }
+      
+         int [] arr = {1,3,4,7,9};
+         for(int i:arr){
+            System.out.println(i);
+         }
+
+        
+    }
+}
