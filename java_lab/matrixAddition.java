b262699deed0d069ed32bf8808368e631a2fec9c
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-02-04 15:32:59 +0530

    ..

---
+++ java_lab/matrixAddition.java
@@ -0,0 +1,53 @@
+package java_lab;
+import java.util.Scanner;
+
+public class matrixAddition {
+    public static void main(String[] args) {
+        
+        int m, n, p, q;
+           Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows of first Matrix:");
+        m = sc.nextInt();
+         System.out.print("Enter the number of column of first Matrix:");
+         n = sc.nextInt();
+         int arr1[][] = new int[m][n];
+         System.out.println("Enter the elements of First Matrix: ");
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                  arr1[i][j] = sc.nextInt();
+            }
+         }
+         // 2nnd Matrix
+         System.out.print("Enter the number of rows of second Matrix:");
+        p = sc.nextInt();
+         System.out.print("Enter the number of column of second Matrix:");
+         q = sc.nextInt();
+         int arr2[][] = new int[p][q];
+         System.out.println("Enter the elements of Second Matrix: ");
+         for(int i=0;i<p;i++){
+            for(int j=0;j<q;j++){
+                  arr2[i][j] = sc.nextInt();
+            }
+         }
+         //Adding Matrix
+         int [][] arr3 = new int[m][n];
+          for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr3[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        System.out.println("Resultant Matrix:");
+         for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+               System.out.print(arr3[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+    
+}
