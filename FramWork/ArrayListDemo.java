f6e74b1e5e2e30ffdd92f0770d9bdb9b9a78ccf7
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-04-08 15:47:49 +0530

    ..

---
+++ FramWork/ArrayListDemo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Character> list = new ArrayList<>(
+            Arrays.asList('A','A','B','B','C','C','C','D','D')
+        );
+
+        LinkedHashSet<Character> set = new LinkedHashSet<>(list);
+        list.clear();
+        list.addAll(set);
+
+        System.out.println(list);  // [A, B, C, D]
+    }
+}
